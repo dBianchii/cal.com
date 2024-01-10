b0a1507ddb43d5f408ada638d09c03085423ff7e
Gabriel Bianchi <gdbianchii@gmail.com>
2024-01-10 10:57:36 -0300

    line-clamp-3

--- packages/ui/components/apps/AppCard.tsx
+++ packages/ui/components/apps/AppCard.tsx
@@ -84,16 +84,7 @@ export function AppCard({ app, credentials, searchText, userAdminTeams }: AppCar
             <span>{props.rating} stars</span> <StarIcon className="ml-1 mt-0.5 h-4 w-4 text-yellow-600" />
             <span className="pl-1 text-subtle">{props.reviews} reviews</span>
           </div> */}
-      <p
-        className="text-default mt-2 flex-grow text-sm"
-        style={{
-          overflow: "hidden",
-          display: "-webkit-box",
-          WebkitBoxOrient: "vertical",
-          WebkitLineClamp: "3",
-        }}>
-        {app.description}
-      </p>
+      <p className="text-default mt-2 line-clamp-3 flex-grow text-sm">{app.description}</p>
 
       <div className="mt-5 flex max-w-full flex-row justify-between gap-2">
         <Button
